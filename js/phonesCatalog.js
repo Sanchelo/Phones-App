b7c4a616aa7e5fa0573f7396a46a35e5dc0062fe
Sanchelo <al.vinokurov92@gmail.com>
2019-04-09 15:59:40 +0300

    Catalog is now rendering dynamically

--- js/phonesCatalog.js
+++ js/phonesCatalog.js
@@ -1,165 +1,192 @@
 export default class PhonesCatalog {
     constructor({element}) {
     this._element = element;
+    this._phones = this._getPhones();
     this._render();
     }
+
     _render() {
         this._element.innerHTML = 
         `
         <ul class="phones">
-        <li class="thumbnail">
-          <a href="#!/phones/motorola-xoom-with-wi-fi" class="thumb">
-            <img alt="Motorola XOOM™ with Wi-Fi" src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-          </a>
+        ${this._phones.map((phone) =>
+          `<li class="thumbnail">
+          <a href="#!/phones/${phone.id}" class="thumb">
+          <img alt="${phone.id}" src="${phone.imageUrl}">
+        </a>
 
-          <div class="phones__btn-buy-wrapper">
-            <a class="btn btn-success">
-              Add
-            </a>
-          </div>
+        <div class="phones__btn-buy-wrapper">
+          <a class="btn btn-success">
+            Add
+          </a>
+        </div>
 
-          <a href="#!/phones/motorola-xoom-with-wi-fi">Motorola XOOM™ with Wi-Fi</a>
-          <p>The Next, Next Generation
-
-            Experience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb).</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/motorola-xoom" class="thumb">
-            <img alt="MOTOROLA XOOM™" src="img/phones/motorola-xoom.0.jpg">
-          </a>
-          <a href="#!/phones/motorola-xoom">MOTOROLA XOOM™</a>
-          <p>The Next, Next Generation
-
-            Experience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb).</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/motorola-atrix-4g" class="thumb">
-            <img alt="MOTOROLA ATRIX™ 4G" src="img/phones/motorola-atrix-4g.0.jpg">
-          </a>
-          <a href="#!/phones/motorola-atrix-4g">MOTOROLA ATRIX™ 4G</a>
-          <p>MOTOROLA ATRIX 4G the world's most powerful smartphone.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/dell-streak-7" class="thumb">
-            <img alt="Dell Streak 7" src="img/phones/dell-streak-7.0.jpg">
-          </a>
-          <a href="#!/phones/dell-streak-7">Dell Streak 7</a>
-          <p>Introducing Dell™ Streak 7. Share photos, videos and movies together. It’s small enough to carry around, big enough to gather around.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/samsung-gem" class="thumb">
-            <img alt="Samsung Gem™" src="img/phones/samsung-gem.0.jpg">
-          </a>
-          <a href="#!/phones/samsung-gem">Samsung Gem™</a>
-          <p>The Samsung Gem™ brings you everything that you would expect and more from a touch display smart phone – more apps, more features and a more affordable price.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/dell-venue" class="thumb">
-            <img alt="Dell Venue" src="img/phones/dell-venue.0.jpg">
-          </a>
-          <a href="#!/phones/dell-venue">Dell Venue</a>
-          <p>The Dell Venue; Your Personal Express Lane to Everything</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/nexus-s" class="thumb">
-            <img alt="Nexus S" src="img/phones/nexus-s.0.jpg">
-          </a>
-          <a href="#!/phones/nexus-s">Nexus S</a>
-          <p>Fast just got faster with Nexus S. A pure Google experience, Nexus S is the first phone to run Gingerbread (Android 2.3), the fastest version of Android yet.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/lg-axis" class="thumb">
-            <img alt="LG Axis" src="img/phones/lg-axis.0.jpg">
-          </a>
-          <a href="#!/phones/lg-axis">LG Axis</a>
-          <p>Android Powered, Google Maps Navigation, 5 Customizable Home Screens</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/samsung-galaxy-tab" class="thumb">
-            <img alt="Samsung Galaxy Tab™" src="img/phones/samsung-galaxy-tab.0.jpg">
-          </a>
-          <a href="#!/phones/samsung-galaxy-tab">Samsung Galaxy Tab™</a>
-          <p>Feel Free to Tab™. The Samsung Galaxy Tab™ brings you an ultra-mobile entertainment experience through its 7” display, high-power processor and Adobe® Flash® Player compatibility.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/samsung-showcase-a-galaxy-s-phone" class="thumb">
-            <img alt="Samsung Showcase™ a Galaxy S™ phone" src="img/phones/samsung-showcase-a-galaxy-s-phone.0.jpg">
-          </a>
-          <a href="#!/phones/samsung-showcase-a-galaxy-s-phone">Samsung Showcase™ a Galaxy S™ phone</a>
-          <p>The Samsung Showcase™ delivers a cinema quality experience like you’ve never seen before. Its innovative 4” touch display technology provides rich picture brilliance, even outdoors</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/droid-2-global-by-motorola" class="thumb">
-            <img alt="DROID™ 2 Global by Motorola" src="img/phones/droid-2-global-by-motorola.0.jpg">
-          </a>
-          <a href="#!/phones/droid-2-global-by-motorola">DROID™ 2 Global by Motorola</a>
-          <p>The first smartphone with a 1.2 GHz processor and global capabilities.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/droid-pro-by-motorola" class="thumb">
-            <img alt="DROID™ Pro by Motorola" src="img/phones/droid-pro-by-motorola.0.jpg">
-          </a>
-          <a href="#!/phones/droid-pro-by-motorola">DROID™ Pro by Motorola</a>
-          <p>The next generation of DOES.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/motorola-bravo-with-motoblur" class="thumb">
-            <img alt="MOTOROLA BRAVO™ with MOTOBLUR™" src="img/phones/motorola-bravo-with-motoblur.0.jpg">
-          </a>
-          <a href="#!/phones/motorola-bravo-with-motoblur">MOTOROLA BRAVO™ with MOTOBLUR™</a>
-          <p>An experience to cheer about.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/motorola-defy-with-motoblur" class="thumb">
-            <img alt="Motorola DEFY™ with MOTOBLUR™" src="img/phones/motorola-defy-with-motoblur.0.jpg">
-          </a>
-          <a href="#!/phones/motorola-defy-with-motoblur">Motorola DEFY™ with MOTOBLUR™</a>
-          <p>Are you ready for everything life throws your way?</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/t-mobile-mytouch-4g" class="thumb">
-            <img alt="T-Mobile myTouch 4G" src="img/phones/t-mobile-mytouch-4g.0.jpg">
-          </a>
-          <a href="#!/phones/t-mobile-mytouch-4g">T-Mobile myTouch 4G</a>
-          <p>The T-Mobile myTouch 4G is a premium smartphone designed to deliver blazing fast 4G speeds so that you can video chat from practically anywhere, with or without Wi-Fi.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/samsung-mesmerize-a-galaxy-s-phone" class="thumb">
-            <img alt="Samsung Mesmerize™ a Galaxy S™ phone" src="img/phones/samsung-mesmerize-a-galaxy-s-phone.0.jpg">
-          </a>
-          <a href="#!/phones/samsung-mesmerize-a-galaxy-s-phone">Samsung Mesmerize™ a Galaxy S™ phone</a>
-          <p>The Samsung Mesmerize™ delivers a cinema quality experience like you’ve never seen before. Its innovative 4” touch display technology provides rich picture brilliance,even outdoors</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/sanyo-zio" class="thumb">
-            <img alt="SANYO ZIO" src="img/phones/sanyo-zio.0.jpg">
-          </a>
-          <a href="#!/phones/sanyo-zio">SANYO ZIO</a>
-          <p>The Sanyo Zio by Kyocera is an Android smartphone with a combination of ultra-sleek styling, strong performance and unprecedented value.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/samsung-transform" class="thumb">
-            <img alt="Samsung Transform™" src="img/phones/samsung-transform.0.jpg">
-          </a>
-          <a href="#!/phones/samsung-transform">Samsung Transform™</a>
-          <p>The Samsung Transform™ brings you a fun way to customize your Android powered touch screen phone to just the way you like it through your favorite themed “Sprint ID Service Pack”.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/t-mobile-g2" class="thumb">
-            <img alt="T-Mobile G2" src="img/phones/t-mobile-g2.0.jpg">
-          </a>
-          <a href="#!/phones/t-mobile-g2">T-Mobile G2</a>
-          <p>The T-Mobile G2 with Google is the first smartphone built for 4G speeds on T-Mobile's new network. Get the information you need, faster than you ever thought possible.</p>
-        </li>
-        <li class="thumbnail">
-          <a href="#!/phones/motorola-charm-with-motoblur" class="thumb">
-            <img alt="Motorola CHARM™ with MOTOBLUR™" src="img/phones/motorola-charm-with-motoblur.0.jpg">
-          </a>
-          <a href="#!/phones/motorola-charm-with-motoblur">Motorola CHARM™ with MOTOBLUR™</a>
-          <p>Motorola CHARM fits easily in your pocket or palm. Includes MOTOBLUR service.</p>
-        </li>
-      </ul>
-
+        <a href="#!/phones/${phone.id}">${phone.name}</a>
+        <p>${phone.snippet}</p>
+          </li>`
+        
+        ).join("")};
+        </ul>
         `
-    }
+    };
+
+    _getPhones() {
+        return [
+          {
+              "age": 0, 
+              "id": "motorola-xoom-with-wi-fi", 
+              "imageUrl": "img/phones/motorola-xoom-with-wi-fi.0.jpg", 
+              "name": "Motorola XOOM\u2122 with Wi-Fi", 
+              "snippet": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
+          }, 
+          {
+              "age": 1, 
+              "id": "motorola-xoom", 
+              "imageUrl": "img/phones/motorola-xoom.0.jpg", 
+              "name": "MOTOROLA XOOM\u2122", 
+              "snippet": "The Next, Next Generation\n\nExperience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb)."
+          }, 
+          {
+              "age": 2, 
+              "carrier": "AT&T", 
+              "id": "motorola-atrix-4g", 
+              "imageUrl": "img/phones/motorola-atrix-4g.0.jpg", 
+              "name": "MOTOROLA ATRIX\u2122 4G", 
+              "snippet": "MOTOROLA ATRIX 4G the world's most powerful smartphone."
+          }, 
+          {
+              "age": 3, 
+              "id": "dell-streak-7", 
+              "imageUrl": "img/phones/dell-streak-7.0.jpg", 
+              "name": "Dell Streak 7", 
+              "snippet": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around."
+          }, 
+          {
+              "age": 4, 
+              "carrier": "Cellular South", 
+              "id": "samsung-gem", 
+              "imageUrl": "img/phones/samsung-gem.0.jpg", 
+              "name": "Samsung Gem\u2122", 
+              "snippet": "The Samsung Gem\u2122 brings you everything that you would expect and more from a touch display smart phone \u2013 more apps, more features and a more affordable price."
+          }, 
+          {
+              "age": 5, 
+              "carrier": "Dell", 
+              "id": "dell-venue", 
+              "imageUrl": "img/phones/dell-venue.0.jpg", 
+              "name": "Dell Venue", 
+              "snippet": "The Dell Venue; Your Personal Express Lane to Everything"
+          }, 
+          {
+              "age": 6, 
+              "carrier": "Best Buy", 
+              "id": "nexus-s", 
+              "imageUrl": "img/phones/nexus-s.0.jpg", 
+              "name": "Nexus S", 
+              "snippet": "Fast just got faster with Nexus S. A pure Google experience, Nexus S is the first phone to run Gingerbread (Android 2.3), the fastest version of Android yet."
+          }, 
+          {
+              "age": 7, 
+              "carrier": "Cellular South", 
+              "id": "lg-axis", 
+              "imageUrl": "img/phones/lg-axis.0.jpg", 
+              "name": "LG Axis", 
+              "snippet": "Android Powered, Google Maps Navigation, 5 Customizable Home Screens"
+          }, 
+          {
+              "age": 8, 
+              "id": "samsung-galaxy-tab", 
+              "imageUrl": "img/phones/samsung-galaxy-tab.0.jpg", 
+              "name": "Samsung Galaxy Tab\u2122", 
+              "snippet": "Feel Free to Tab\u2122. The Samsung Galaxy Tab\u2122 brings you an ultra-mobile entertainment experience through its 7\u201d display, high-power processor and Adobe\u00ae Flash\u00ae Player compatibility."
+          }, 
+          {
+              "age": 9, 
+              "carrier": "Cellular South", 
+              "id": "samsung-showcase-a-galaxy-s-phone", 
+              "imageUrl": "img/phones/samsung-showcase-a-galaxy-s-phone.0.jpg", 
+              "name": "Samsung Showcase\u2122 a Galaxy S\u2122 phone", 
+              "snippet": "The Samsung Showcase\u2122 delivers a cinema quality experience like you\u2019ve never seen before. Its innovative 4\u201d touch display technology provides rich picture brilliance, even outdoors"
+          }, 
+          {
+              "age": 10, 
+              "carrier": "Verizon", 
+              "id": "droid-2-global-by-motorola", 
+              "imageUrl": "img/phones/droid-2-global-by-motorola.0.jpg", 
+              "name": "DROID\u2122 2 Global by Motorola", 
+              "snippet": "The first smartphone with a 1.2 GHz processor and global capabilities."
+          }, 
+          {
+              "age": 11, 
+              "carrier": "Verizon", 
+              "id": "droid-pro-by-motorola", 
+              "imageUrl": "img/phones/droid-pro-by-motorola.0.jpg", 
+              "name": "DROID\u2122 Pro by Motorola", 
+              "snippet": "The next generation of DOES."
+          }, 
+          {
+              "age": 12, 
+              "carrier": "AT&T", 
+              "id": "motorola-bravo-with-motoblur", 
+              "imageUrl": "img/phones/motorola-bravo-with-motoblur.0.jpg", 
+              "name": "MOTOROLA BRAVO\u2122 with MOTOBLUR\u2122", 
+              "snippet": "An experience to cheer about."
+          }, 
+          {
+              "age": 13, 
+              "carrier": "T-Mobile", 
+              "id": "motorola-defy-with-motoblur", 
+              "imageUrl": "img/phones/motorola-defy-with-motoblur.0.jpg", 
+              "name": "Motorola DEFY\u2122 with MOTOBLUR\u2122", 
+              "snippet": "Are you ready for everything life throws your way?"
+          }, 
+          {
+              "age": 14, 
+              "carrier": "T-Mobile", 
+              "id": "t-mobile-mytouch-4g", 
+              "imageUrl": "img/phones/t-mobile-mytouch-4g.0.jpg", 
+              "name": "T-Mobile myTouch 4G", 
+              "snippet": "The T-Mobile myTouch 4G is a premium smartphone designed to deliver blazing fast 4G speeds so that you can video chat from practically anywhere, with or without Wi-Fi."
+          }, 
+          {
+              "age": 15, 
+              "carrier": "US Cellular", 
+              "id": "samsung-mesmerize-a-galaxy-s-phone", 
+              "imageUrl": "img/phones/samsung-mesmerize-a-galaxy-s-phone.0.jpg", 
+              "name": "Samsung Mesmerize\u2122 a Galaxy S\u2122 phone", 
+              "snippet": "The Samsung Mesmerize\u2122 delivers a cinema quality experience like you\u2019ve never seen before. Its innovative 4\u201d touch display technology provides rich picture brilliance,even outdoors"
+          }, 
+          {
+              "age": 16, 
+              "carrier": "Sprint", 
+              "id": "sanyo-zio", 
+              "imageUrl": "img/phones/sanyo-zio.0.jpg", 
+              "name": "SANYO ZIO", 
+              "snippet": "The Sanyo Zio by Kyocera is an Android smartphone with a combination of ultra-sleek styling, strong performance and unprecedented value."
+          }, 
+          {
+              "age": 17, 
+              "id": "samsung-transform", 
+              "imageUrl": "img/phones/samsung-transform.0.jpg", 
+              "name": "Samsung Transform\u2122", 
+              "snippet": "The Samsung Transform\u2122 brings you a fun way to customize your Android powered touch screen phone to just the way you like it through your favorite themed \u201cSprint ID Service Pack\u201d."
+          }, 
+          {
+              "age": 18, 
+              "id": "t-mobile-g2", 
+              "imageUrl": "img/phones/t-mobile-g2.0.jpg", 
+              "name": "T-Mobile G2", 
+              "snippet": "The T-Mobile G2 with Google is the first smartphone built for 4G speeds on T-Mobile's new network. Get the information you need, faster than you ever thought possible."
+          }, 
+          {
+              "age": 19, 
+              "id": "motorola-charm-with-motoblur", 
+              "imageUrl": "img/phones/motorola-charm-with-motoblur.0.jpg", 
+              "name": "Motorola CHARM\u2122 with MOTOBLUR\u2122", 
+              "snippet": "Motorola CHARM fits easily in your pocket or palm.  Includes MOTOBLUR service."
+          }
+      ]
+      
+      };
+
 }
--- js/phonesPage.js
+++ js/phonesPage.js
@@ -1,6 +1,7 @@
 import Sort from "./sort.js";
 import Cart from "./cart.js";
 import PhonesCatalog from "./phonesCatalog.js";
+import PhoneViewer from "./phoneViewer.js";
 
 export default class PhonesPage {
     constructor({element}) {
@@ -9,6 +10,7 @@ export default class PhonesPage {
       new Sort({element: document.querySelector(".sort")});
       new Cart({element: document.querySelector(".cart")});
       new PhonesCatalog({element: document.querySelector(".phones-catalog")});
+      new PhoneViewer({element: document.querySelector(".phone-viewer")});
     }
     _render() {
         this._element.innerHTML = 
@@ -30,6 +32,7 @@ export default class PhonesPage {
           <div class="col-md-10 phones-catalog">
           
           </div>
+          <div style="display: none" class="phone-viewer"></div>
         </div>
       </div>
     `;
