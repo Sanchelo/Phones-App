c5ab2189309d9054a20a37e92633bac06b3e3e9e
Sanchelo <al.vinokurov92@gmail.com>
2019-04-11 20:40:44 +0300

    PhoneService and events were added

--- js/phonesPage.js
+++ js/phonesPage.js
@@ -2,15 +2,18 @@ import Sort from "./sort.js";
 import Cart from "./cart.js";
 import PhonesCatalog from "./phonesCatalog.js";
 import PhoneViewer from "./phoneViewer.js";
+import phoneService from "./phoneService.js";
 
 export default class PhonesPage {
     constructor({element}) {
       this._element = element;
-        this._render();
-      new Sort({element: document.querySelector(".sort")});
-      new Cart({element: document.querySelector(".cart")});
-      new PhonesCatalog({element: document.querySelector(".phones-catalog")});
-      new PhoneViewer({element: document.querySelector(".phone-viewer")});
+      this._render();
+      this._filter = new Sort({element: document.querySelector(".sort")});
+      this._cart = new Cart({element: document.querySelector(".cart")});
+      this._phonesCatalog = new PhonesCatalog({element: document.querySelector(".phones-catalog"), phones: phoneService.getAllPhones(), onPhoneSelected: (phoneId) => {
+        const selectedPhone = this._phonesCatalog._getById(phoneId); console.log(selectedPhone)
+      }});
+      this._phoneViewer = new PhoneViewer({element: document.querySelector(".phone-viewer")});
     }
     _render() {
         this._element.innerHTML = 
